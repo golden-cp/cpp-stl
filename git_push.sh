44478b567d7f478396baafbccfbc1b048a0301a6
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-08-07 01:23:13 +0530

    first commit

---
+++ git_push.sh
@@ -0,0 +1,7 @@
+echo "# cpp-demo"
+git init
+git add git_push.sh  hello_world.cpp  README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/golden-cp/cpp-demo.git
+git push -u origin main
---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(string s){
+    cout<<s<<endl;
+}
+int main(){
+    cout<<"Hello World!!"<<endl;
+    string s = "Rahul";
+    print(s);
+    return 0;
+}
