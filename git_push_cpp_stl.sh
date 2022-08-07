e6364a3f24c1f5dcdefc6449aa7040fac767bac6
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-08-07 13:50:27 +0530

    first commit

--- git_push_cpp_stl.sh
+++ git_push_cpp_stl.sh
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add github_spp_stl https://github.com/golden-cp/cpp-stl.git
+git remote add github_cpp_stl https://github.com/golden-cp/cpp-stl.git
 git push -u github_cpp_stl main
-git branch --set-upstream main github_cpp_stl/main
+# git branch --set-upstream-to main github_cpp_stl
